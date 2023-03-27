d06a81d823f7aa392202bd398c8b7992b49ffbff
Guilherme Henrique <103789281+Guihenrique62@users.noreply.github.com>
2023-03-27 17:42:33 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,11 +1,58 @@
-const playerLi = document.querySelectorAll('.playerli')
-const addBtn = document.querySelectorAll('.addBtn')
+const playerDisp = document.querySelectorAll('.playerDisp')
+const playerEsc = document.querySelectorAll('.playerEsc')
+const lists = document.getElementById('list')
 
-for(let i = 0; i <= playerLi.length; i++){
-    playerLi[i].addEventListener('mouseover',()=>{
+const createAddBtn = document.createElement('button')
+createAddBtn.classList = 'addBtn'
+createAddBtn.id = 'addBtn'
+createAddBtn.innerText = '+'
+
+const createDelBtn = document.createElement('button')
+createDelBtn.classList = 'delBtn'
+createDelBtn.id = 'delBtn'
+createDelBtn.innerText = '-'
+
+
+//Cria botão Del
+playerEsc.forEach(player => {
+    player.addEventListener('mouseover', ()=>{
+        player.append(createDelBtn)
+        const delBtn = document.getElementById('delBtn')
+        delBtn.addEventListener('click',()=>{
+            console.log('teste')
+        })
+    })
+});
+
+  //Cria botão Add
+playerDisp.forEach(player => {
+    player.addEventListener('mouseover', ()=>{
+        player.append(createAddBtn)
+    })
+    player.addEventListener('mouseout', ()=>{
+        player.removeChild(createAddBtn)
+    })
+});
+
+
+
+/*
+//Add event player disponiveis
+for(let i = 0; i <= playerDisp.length; i++){
+    playerDisp[i].addEventListener('mouseover',()=>{
     addBtn[i].style.display = 'block'
     })
-    playerLi[i].addEventListener('mouseout',()=>{
+    playerDisp[i].addEventListener('mouseout',()=>{
        addBtn[i].style.display = 'none'
     })
 }
+
+//Add event player Escalado
+for(let j = 0; j <= playerEsc.length; j++){
+    playerEsc[j].addEventListener('mouseover',()=>{
+       delBtn[j].style.display = 'block'
+    })
+    playerEsc[j].addEventListener('mouseout',()=>{
+       delBtn[j].style.display = 'none'
+    })
+}*/
