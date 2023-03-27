f3749dcc519b9b8e395137dd463b2d86d656ab7b
Guilherme Henrique <103789281+Guihenrique62@users.noreply.github.com>
2023-03-27 14:58:31 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,7 +1,11 @@
 const playerLi = document.querySelectorAll('.playerli')
+const addBtn = document.querySelectorAll('.addBtn')
 
 for(let i = 0; i <= playerLi.length; i++){
     playerLi[i].addEventListener('mouseover',()=>{
-
+    addBtn[i].style.display = 'block'
+    })
+    playerLi[i].addEventListener('mouseout',()=>{
+       addBtn[i].style.display = 'none'
     })
 }
