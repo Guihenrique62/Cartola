7777f055c3747851376ac744064182b491a69e1e
Guilherme Henrique <103789281+Guihenrique62@users.noreply.github.com>
2023-03-29 14:56:06 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,11 +1,88 @@
-const playerLi = document.querySelectorAll('.playerli')
-const addBtn = document.querySelectorAll('.addBtn')
+const playerDisp = document.querySelectorAll('.playerDisp')
+const playerEsc = document.querySelectorAll('.playerEsc')
+const listEsc = document.getElementById('listEsc')
+const listDisp = document.getElementById('listDisp')
 
-for(let i = 0; i <= playerLi.length; i++){
-    playerLi[i].addEventListener('mouseover',()=>{
-    addBtn[i].style.display = 'block'
-    })
-    playerLi[i].addEventListener('mouseout',()=>{
-       addBtn[i].style.display = 'none'
-    })
+const createAddBtn = document.createElement('button')
+createAddBtn.classList = 'addBtn'
+createAddBtn.id = 'addBtn'
+createAddBtn.innerText = '+'
+
+const createDelBtn = document.createElement('button')
+createDelBtn.classList = 'delBtn'
+createDelBtn.id = 'delBtn'
+createDelBtn.innerText = '-'
+
+const movedPlayers = []
+
+function addElementDisp(text){
+    const newLi = document.createElement('li')
+    newLi.classList.add('butt')
+    const newSpan = document.createElement('span');
+    newSpan.classList.add('spans','playerDisp')
+    const newP = document.createElement('p');
+    newP.innerText = text
+
+    newSpan.append(newP);
+    newLi.append(newSpan)
+    listDisp.append(newLi);
 }
+function addElementEsc(text){
+    const newLi = document.createElement('li')
+    newLi.classList.add('butt')
+    const newSpan = document.createElement('span');
+    newSpan.classList.add('spans','playerEsc')
+    const newP = document.createElement('p');
+    newP.innerText = text
+
+    newSpan.append(newP);
+    newLi.append(newSpan)
+    listEsc.append(newLi);
+}
+
+playerDisp.forEach(player => {
+    const addBtn = createAddBtn
+    function handleAddBtnClick() {
+        const playerName = player.firstChild.nextSibling.textContent
+        if (!movedPlayers.includes(playerName)) {
+            addElementEsc(playerName)
+            movedPlayers.push(playerName)
+        }
+        player.remove()
+    }
+
+    player.addEventListener('mouseenter',()=>{
+        player.append(addBtn)
+        addBtn.addEventListener('click', handleAddBtnClick)
+    })
+
+
+    player.addEventListener('mouseleave', () => {
+        player.removeChild(addBtn)
+        addBtn.removeEventListener('click', handleAddBtnClick)
+    })
+});
+
+playerEsc.forEach(player => {
+    const delBtn = createDelBtn
+    function handleDelBtnClick() {
+        const playerName = player.firstChild.nextSibling.textContent
+        if (!movedPlayers.includes(playerName)) {
+            addElementDisp(playerName)
+            movedPlayers.push(playerName)
+        }
+        player.remove()
+    }
+
+    player.addEventListener('mouseenter',()=>{
+        player.append(delBtn)
+        delBtn.addEventListener('click', handleDelBtnClick)
+    })
+
+
+    player.addEventListener('mouseleave', () => {
+        player.removeChild(delBtn)
+        delBtn.removeEventListener('click', handleDelBtnClick)
+    })
+});
+
