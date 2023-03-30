c46a8a8fb44e2c0e26777ee9d1143b3233f968bd
Guilherme Henrique <103789281+Guihenrique62@users.noreply.github.com>
2023-03-29 21:28:58 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
+//Variáveis dos elementos
 const playerDisp = document.querySelectorAll('.playerDisp')
 const playerEsc = document.querySelectorAll('.playerEsc')
 const listEsc = document.getElementById('listEsc')
 const listDisp = document.getElementById('listDisp')
 
+//Criação dos botôes
 const createAddBtn = document.createElement('button')
 createAddBtn.classList = 'addBtn'
 createAddBtn.id = 'addBtn'
@@ -13,8 +15,12 @@ createDelBtn.classList = 'delBtn'
 createDelBtn.id = 'delBtn'
 createDelBtn.innerText = '-'
 
+// Array que irá registrar todos os player que foram modificados
 const movedPlayers = []
+const newMovedPlayers = []
+const newMovedPlayers1 = []
 
+//funçoes para criar um novo span em cada list
 function addElementDisp(text){
     const newLi = document.createElement('li')
     newLi.classList.add('butt')
@@ -22,6 +28,26 @@ function addElementDisp(text){
     newSpan.classList.add('spans','playerDisp')
     const newP = document.createElement('p');
     newP.innerText = text
+
+    function handleAddBtnClick() {
+        const playerName = newLi.firstChild.firstChild.textContent
+        if (!newMovedPlayers1.includes(playerName)) {
+            addElementEsc(playerName)
+            newMovedPlayers1.push(playerName)
+        }
+        newLi.remove()
+    }
+
+    newLi.addEventListener('mouseenter', () =>{
+        const addBtn = createAddBtn
+        newLi.firstChild.append(addBtn)
+        addBtn.addEventListener('click', handleAddBtnClick)
+    })
+    newLi.addEventListener('mouseleave', () => {
+        const addBtn = createAddBtn
+        newLi.firstChild.removeChild(addBtn)
+        addBtn.removeEventListener('click', handleAddBtnClick)
+    })
 
     newSpan.append(newP);
     newLi.append(newSpan)
@@ -35,10 +61,31 @@ function addElementEsc(text){
     const newP = document.createElement('p');
     newP.innerText = text
 
+    function handleDelBtnClick() {
+        const playerName = newLi.firstChild.firstChild.textContent
+        if (!newMovedPlayers.includes(playerName)) {
+            addElementDisp(playerName)
+            newMovedPlayers.push(playerName)
+        }
+        newLi.remove()
+    }
+
+    newLi.addEventListener('mouseenter', () =>{
+        const delBtn = createDelBtn
+        newLi.firstChild.append(delBtn)
+        delBtn.addEventListener('click', handleDelBtnClick)
+    })
+    newLi.addEventListener('mouseleave', () => {
+        const delBtn = createDelBtn
+        newLi.firstChild.removeChild(delBtn)
+        delBtn.removeEventListener('click', handleDelBtnClick)
+    })
     newSpan.append(newP);
     newLi.append(newSpan)
     listEsc.append(newLi);
 }
+
+//forEach iterando sobre cada elemento da node list, adicionando os eventos
 
 playerDisp.forEach(player => {
     const addBtn = createAddBtn
@@ -61,6 +108,7 @@ playerDisp.forEach(player => {
         player.removeChild(addBtn)
         addBtn.removeEventListener('click', handleAddBtnClick)
     })
+    
 });
 
 playerEsc.forEach(player => {
@@ -85,4 +133,5 @@ playerEsc.forEach(player => {
         delBtn.removeEventListener('click', handleDelBtnClick)
     })
 });
+
 
